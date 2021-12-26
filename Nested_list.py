0ea999de5db9e9ed6a6bd6cbaa244d6280c71e8b
Maddukuri Siva Ram Prasad <93768335+M-S-R-P@users.noreply.github.com>
2021-12-26 14:43:24 +0530

    Improper nested list code

--- Nested_list.py
+++ Nested_list.py
@@ -1,11 +1,21 @@
-name = []
-score = []
-for i in range(int(input())):
-    print(i)
-    name[i] = input()
-    print(name)
-    score[i] = float(input())
-#testing git 
-#committing changes
+name=[]
+score=[]
+for _ in range(int(input())):
+    name.append(input())
+    score.append(float(input()))
 
-print(name)
+nstlst = []
+for i in range(len(name)) :
+    nstlst.insert(i , [name[i], score[i]]) 
+nstlst.sort(key = lambda x:x[1], reverse = True)
+
+least = nstlst[-1][1]
+
+while (nstlst[-1][1]) == least :
+    nstlst.pop()
+
+last = 1
+print(nstlst[-1][0])
+while nstlst[-last][1] == nstlst[-last-1][1] :
+    print(nstlst[-last-1][0])
+    last += 1
