cc7368f6a266a60e5de10d05de5293225840e7b7
Maddukuri Siva Ram Prasad <93768335+M-S-R-P@users.noreply.github.com>
2022-09-10 09:33:04 +0530

    Small Correction to improved alphabet-rangoli

--- alphabet-rangoli-2.py
+++ alphabet-rangoli-2.py
@@ -2,8 +2,8 @@ def alphrang(a) :
     def sub_alph(b) :
         lst = []
         d = {1:'a',2:'b',3:'c',4:'d',5:'e',6:'f',7:'g',8:'h',9:'i',10:'j',11:'k',12:'l',13:'m',14:'n',15:'o',16:'p',17:'q',18:'r',19:'s',20:'t',21:'u',22:'v',23:'w',24:'x',25:'y',26:'z'}
-        for i in range(b+1,0,-1) :
-            lst.append(d[i])
+        for i in range(1,b+2) :
+            lst.append(d[a-i+1])
         lst += lst[-2::-1]
         print('-'.join(lst).center(4*a-3,'-'))
     for i in range(a) :
